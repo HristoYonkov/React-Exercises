1107186ec8a2cd76461565260619a98e1c738b7f
Hristo Yonkov <hristopturs@gmail.com>
2023-08-16 18:23:00 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Outlet, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Home from './containers/Home/Home';
+import UseReducer from './containers/UseReducer/UseReducer';
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Root />} >
+        <Route index element={<Home />} />
+        <Route path='/useReducer' element={<UseReducer />} />
+      </Route>
+    )
+  );
+
   return (
     <div className="App">
-      <h1>sadas</h1>
+      <RouterProvider router={router}/>
     </div>
   );
 }
 
 export default App;
+
+const Root = () => {
+
+  return <>
+    <header>
+      <nav>
+        <Link to={'/'} >Home</Link>
+        <Link to={'/useReducer'} >UseReducer</Link>
+      </nav>
+    </header>
+
+    <div>
+      <Outlet />
+    </div>
+  </>
+}
