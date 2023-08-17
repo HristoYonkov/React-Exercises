6ae11c6ed59592028258887cb39106bf15d6d1bf
Hristo Yonkov <hristopturs@gmail.com>
2023-08-17 17:57:46 +0300

    apply the use reducer logic!

--- client/src/containers/UseReducer/UseReducer.js
+++ client/src/containers/UseReducer/UseReducer.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { ...state, value: state.value + 1 }
+      break;
+    case 'decrement':
+      return { ...state, value: state.value - 1 }
+      break;
+    case 'ifEven':
+      if (state.value % 2 === 0) {
+        return { ...state, showText: true }
+      } else {
+        return { ...state, showText: false }
+      }
+      break;
+    default:
+      return state;
+      break;
+  }
+}
 
 const UseReducer = () => {
+  const [state, dispatch] = useReducer(reducer, { value: 0, showText: true });
+
   return (
-    <h1>UseReducer</h1>
+    <>
+      <h1>UseReducer</h1>
+      <h2>{state.value}</h2>
+
+      <button onClick={() => {
+        dispatch({ type: 'increment' });
+        dispatch({ type: 'ifEven' });
+      }}
+      >
+        Increment
+      </button >
+      
+      <button onClick={() => {
+        dispatch({ type: 'decrement' });
+        dispatch({ type: 'ifEven' });
+      }}
+      >
+        Decrement
+      </button>
+
+      {state.showText && <p>This number is even</p>}
+    </>
   )
 }
 
