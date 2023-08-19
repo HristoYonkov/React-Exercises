c51052005c550ac66445d03e2b26beefe7f5ceed
Hristo Yonkov <hristopturs@gmail.com>
2023-08-19 17:36:33 +0300

    Update Button.js

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,13 +1,22 @@
-import React, { forwardRef, useState } from 'react'
+import React, { forwardRef, useImperativeHandle, useState } from 'react'
 
-const Button = forwardRef((props, ref) => {
-    const [toggle, setToggle] = useState(false)
+const Button = forwardRef(({}, ref) => {
+    const [toggle, setToggle] = useState(false);
+    
+    useImperativeHandle(ref, () => ({
+        // Alter way for setting the toggle state in the child component,
+        // instead of setting it in the parent state!
+        alterToggle() {
+            setToggle(!toggle);
+        }
+    }));
+
     return (
         <>
             <button
-                onClick={() => {
-                    setToggle(!toggle);
-                }}
+                // onClick={() => {
+                //     setToggle(!toggle);
+                // }}
             >
                 Button From Child
             </button>
