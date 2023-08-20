5f4453001195208a1d8b654a2bcfb01787194c69
Hristo Yonkov <hristopturs@gmail.com>
2023-08-20 15:18:15 +0300

    Update UseContext.js

--- client/src/containers/UseContext/UseContext.js
+++ client/src/containers/UseContext/UseContext.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
+import Login from '../../components/Login/Login'
+import User from '../../components/User/User'
+
+export const useCTXContext = createContext(null);
 
 const UseContext = () => {
-  return (
-    <h1>UseContext</h1>
-  )
+    const [username, setUsername] = useState('');
+
+    return (
+        <useCTXContext.Provider value={{ username, setUsername }}>
+            <h1>UseContext</h1>
+            <Login /> <User />
+        </useCTXContext.Provider>
+    )
 }
 
 export default UseContext
