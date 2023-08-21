58555e181367ce2f45cdefd6a553e7c0c336e0cc
Hristo Yonkov <hristopturs@gmail.com>
2023-08-21 17:09:23 +0300

    Update UseCallback.js

--- client/src/containers/UseCallback/UseCallback.js
+++ client/src/containers/UseCallback/UseCallback.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import Child from '../../components/Child/Child';
+// Simmilar as 'useMemo' hook but instead of returning value,
+// it returns and 'memorize' entire function!
 
 const UseCallback = () => {
+  const [data, setData] = useState('Data from the Parent!');
+  const [toggle, setToggle] = useState(false);
+
+  const returnComment = useCallback((name) => {
+    return data + name;
+  }, [data]);
+
+
   return (
-    <h1>UseCallback</h1>
+    <>
+      <h1>UseCallback</h1>
+      <Child returnComment={returnComment} />
+
+      <button
+        onClick={() => { setToggle(!toggle) }}
+      >
+        Toggle
+      </button>
+      {toggle && <h1>Toggle!</h1>}
+    </>
   )
 }
 
