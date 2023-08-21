ffec5d95cc2ec52624f4e743e1e1c56cf3f1687d
Hristo Yonkov <hristopturs@gmail.com>
2023-08-21 12:32:51 +0300

    Update UseMemo.js

--- client/src/containers/UseMemo/UseMemo.js
+++ client/src/containers/UseMemo/UseMemo.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const UseMemo = () => {
+    const [data, setData] = useState(null);
+    const [toggle, setToggle] = useState(false);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/comments')
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data);
+            })
+            
+        }, []);
+        
+        console.log(data);
     return (
         <>
             <h1>UseMemo</h1>
