e60f7957a70f45336148dedfa4b3f32375420574
Hristo Yonkov <hristopturs@gmail.com>
2023-08-22 16:24:41 +0300

    Update UseContext.js

--- client/src/containers/UseContext/UseContext.js
+++ client/src/containers/UseContext/UseContext.js
@@ -2,6 +2,9 @@ import React, { createContext, useState } from 'react'
 import Login from '../../components/Login/Login'
 import User from '../../components/User/User'
 
+// useContext Helps Us Avoid Prop Drilling
+// In React, we want to avoid the following problem of creating multiple props
+// to pass data down two or more levels from a parent component.
 export const useCTXContext = createContext(null);
 
 const UseContext = () => {
