dbd0bf5750d98edd469931a4b65ee7941abb4f58
Hristo Yonkov <hristopturs@gmail.com>
2023-08-22 16:19:07 +0300

    Update UseCallback.js

--- client/src/containers/UseCallback/UseCallback.js
+++ client/src/containers/UseCallback/UseCallback.js
@@ -3,6 +3,11 @@ import Child from '../../components/Child/Child';
 // Simmilar as 'useMemo' hook but instead of returning value,
 // it returns and 'memorize' entire function!
 
+// useCallback Prevents Callbacks from Being Recreated
+// useCallback is a hook that is used for improving our component performance.
+// useCallback memoizes our callback functions, so they not recreated on every re-render.
+// Using useCallback correctly can improve the performance of our app.
+
 const UseCallback = () => {
   const [data, setData] = useState('Data from the Parent!');
   const [toggle, setToggle] = useState(false);
