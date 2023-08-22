267037f043652d2e88fe9c01e3da9ab992dcf54c
Hristo Yonkov <hristopturs@gmail.com>
2023-08-22 16:23:04 +0300

    Update UseMemo.js

--- client/src/containers/UseMemo/UseMemo.js
+++ client/src/containers/UseMemo/UseMemo.js
@@ -2,6 +2,15 @@ import React, { useEffect, useMemo, useState } from 'react'
 // Used for complex computations when certain function needs more time!
 // And for example useMemo hook meorize the ''value'' until data has changed
 // So the component will safe more time next time when re-render.
+
+// useMemo Can Improve Expensive Operations
+// useMemo is very similar to useCallback and helps improve performance.
+// But instead of being for callbacks, it is for storing the results of expensive operations.
+// useMemo allows us to memoize, or remember the result of expensive operations -
+// when they have already been made for certain inputs.
+// Memoization means that if a calculation has been done before with a given input,
+// there's no need to do it again, because we already have the stored result of that operation.
+
 const UseMemo = () => {
     const [data, setData] = useState(null);
     const [toggle, setToggle] = useState(false);
